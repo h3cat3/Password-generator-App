0050e3504b5230131cc53d4c1d10fcb8755981c2
h3cat3 <dumitrescu.c.e@gmail.com>
2026-02-01 15:36:03 +0000

    checkbox
    custom styled the checkbox

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const lengthNo = document.getElementById('length-value');
 const lengthInput = document.getElementById('length');
 
+const uppercase = document.getElementById('uppercase');
+const lowercase = document.getElementById('lowercase');
+const numbers = document.getElementById('numbers');
+const symbols = document.getElementById('symbols');
+
+const checkBox = document.querySelectorAll('.sqare');
+const checkImg = document.querySelectorAll('.checked');
+
 lengthInput.addEventListener('input', () => {
     lengthNo.innerText = lengthInput.value;
     updateGradient();
@@ -14,5 +22,20 @@ function updateGradient() {
   lengthInput.style.backgroundColor = 'var(--grey-850)';
 }
 
-
+uppercase.addEventListener('click',() => {
+    checkBox[0].classList.toggle('hidden')
+    checkImg[0].classList.toggle('hidden');
+})
+lowercase.addEventListener('click',() => {
+    checkBox[1].classList.toggle('hidden')
+    checkImg[1].classList.toggle('hidden');
+})
+numbers.addEventListener('click',() => {
+    checkBox[2].classList.toggle('hidden')
+    checkImg[2].classList.toggle('hidden');
+})
+symbols.addEventListener('click',() => {
+    checkBox[3].classList.toggle('hidden')
+    checkImg[3].classList.toggle('hidden');
+})
 updateGradient();
